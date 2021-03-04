d8e07f5c5b365e529875f85f0248a50b7f484b0b
wanderleysoliveira <wanderley_santos@ymail.com>
2021-03-04 15:09:15 -0300

    adicionando texto com acentos

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body>
 <h1>Bem vindo a turma 15A</h1>
 <h3>Testando mais uma vez o commit</h3>
-<img src="https://images6.websiteseguro.com/fotos/551-ranking-voto-popular-2019/10-cabana-voto-popular-2019.jpg" alt="">
+<h6>Testando Acentos ÁÕÀ</h6>
+
+<img width=50% src="https://images6.websiteseguro.com/fotos/551-ranking-voto-popular-2019/10-cabana-voto-popular-2019.jpg">
+    
+</body>
+</html>
