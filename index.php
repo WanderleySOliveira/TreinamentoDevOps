162812ceaf73db191ac2472c08da54d556d55ed0
wanderleysoliveira <wanderley_santos@ymail.com>
2021-03-04 14:54:27 -0300

    adicionando img

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1>Bem vindo a turma 15A</h1>
-<h2> testando commit git </h2>
 <h3>Testando mais uma vez o commit</h3>
+<img src="https://images6.websiteseguro.com/fotos/551-ranking-voto-popular-2019/10-cabana-voto-popular-2019.jpg" alt="">
